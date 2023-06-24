33d088ed1b880adbf81bee54763c8764766c704e
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-06-24 08:54:23 +0500

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,9 @@ import "./Navbar.scss";
 import Navlist from "./Navlist";
 
 const Navbar = () => {
+  const openBar = ()=>{
+    document.body.classList.toggle("open-bar")
+  }
   return (
     <header className="header">
       <div className="header__nav  container">
@@ -11,11 +14,17 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
           <ul className="nav_list">
             <Navlist />
+            <label className="close" onClick={openBar}>
+            <i class="fa-solid fa-x"></i>
+            </label>
           </ul>
         </nav>
         <div className="nav_languages">
           <button className="ru">RU</button>
           <button className="en">EN</button>
+          <label className="open" onClick={openBar}>
+          <i class="fa-solid fa-bars"></i>
+          </label>
         </div>
       </div>
     </header>
